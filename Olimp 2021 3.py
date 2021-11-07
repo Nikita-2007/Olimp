cb7a3d47fb06056618529be128c67ce88856b1b7
Nikita-2007 <nikita2007ua@gmail.com>
2021-11-07 21:05:48 +0300

    Update Olimp 2021 3.py

--- Olimp 2021 3.py
+++ Olimp 2021 3.py
@@ -1,12 +1,30 @@
 N, M = map(int, input().split())
-sed, x, y, arr, arr2 = 0, 0, 0, list(), list()
+arr = []
+sed = 0
+arrMin = []
+
+for y in range(N):
+    line = list(map(int, input().split()))
+    arr.append(line)
+    arrMin.append(min(arr[y]))
+
+sed = max(arrMin)
+sedY = arrMin.index(sed)
 for i in range(N):
-    temparr = list(map(int, input().split()))
-    arr.append(min(temparr))
-    arr2.append(temparr)
-    sed = max(arr)
-    y = arr.index(sed)
-    if arr2[i][y] == sed:
-        x = i
-print(sed)
-print(x, y)
+    if arr[sedY][i] == sed:
+        sedX = i
+        break
+
+if sed != 0:
+    print(sed)
+    print(sedY, sedX)
+else:
+    print(0)
+
+
+#5 6
+#2 3 2 3 2 5
+#1 2 1 2 1 3
+#9 8 9 8 5 6
+#3 4 3 4 3 4
+#2 1 2 1 2 3
