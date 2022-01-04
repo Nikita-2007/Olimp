baef8542756a205be1febec8ff154396833cd975
Nikita-2007 <nikita2007ua@gmail.com>
2022-01-04 17:08:25 +0300

    2

--- Region 2021/2021-2.py
+++ Region 2021/2021-2.py
@@ -1,3 +1,37 @@
-d, m, w = map(int, input().split())
-i, j, k = map(int, input().split())
-print(chr(((k-1) * m * d + (j-1) * d + (i-1)) % w+ord('a')))
+t = int(input())
+for i in range(t):
+    n, m = map(int, input().split())
+    k = n*m
+    S = int((1 + k)*k/2)
+    pS = S / 2
+    V = m // 2
+    col1 = int((1 + (n - 1) * m + 1) * n / 2)
+    colV = int((V + (n - 1) * m + V) * n / 2)
+    colV2 = int(((V+1) + (n - 1) * m + (V+1)) * n / 2)
+    vS = int((col1 + colV) * V / 2)
+    vS2 = int((col1 + colV2) * (V+1) / 2)
+    vR = abs(pS - vS)
+    vR2 = abs(pS - vS2)
+    if vR <= vR2:
+        minV = vR
+        col = V+1
+    else:
+        minV = vR2
+        col = V+2
+
+    H = round(n * 0.7066667) #ДОДЕЛАТЬ
+    hS = int((H * m / 2) * (H*m+1))
+    hS2 = int(((H+1) * m / 2) * ((H+1)*m+1))
+    hR = abs(pS - hS)
+    hR2 = abs(pS - hS2)
+    if hR <= hR2:
+        minH = hR
+        row = H+1
+    else:
+        minH = hR2
+        row = H+2
+
+    if minH <= minV:
+        print('H', row)
+    else:
+        print('V', col)
