40185100b58862fc19befcaf8997780ac30ae779
Nikita-2007 <nikita2007ua@gmail.com>
2022-01-13 21:14:39 +0300

    Update 20-4.py

--- 20-4.py
+++ 20-4.py
@@ -1,9 +1,22 @@
 N = int(input())
 T = int(input())
-maxi = start = 0
-
+arr = [0] * ((N-1) //2)
 for i in range(T):
     K, S = map(int, input().split())
-    maxi = (N-(K*2-1))*4
-    start = (maxi - S+1) % maxi
-    print(start, maxi)
+    arr[K-1] = (arr[K-1] + S) % ((N-1) * 4 -(K-1)*8)
+matrix = [0]*N
+for i in range(N):
+    matrix[i] = [1] * N
+
+for i in range(len(arr)):
+    tN = N-i*2
+    L = (tN - 1) * 4
+    for x in range(tN):
+        matrix[i][i + x] = (x-arr[i]) % L +1
+        matrix[N-i-1][i + x] = (L - (tN-2)-x-arr[i]-1) % L +1
+    for y in range(tN-2):
+        matrix[y+i+1][i] = (L-y-arr[i]-1) % L +1
+        matrix[y+i+1][N-i-1] = (tN+y-arr[i]) % L +1
+for i in matrix:
+    print(*i)
+
