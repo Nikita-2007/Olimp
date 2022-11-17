a68b9808802a5608826aed1fea097f5765474284
Nikita-2007 <nikita2007ua@gmail.com>
2022-11-17 21:21:03 +0300

    5

--- Training City2022/5.py
+++ Training City2022/5.py
@@ -1,22 +1,27 @@
-n, m = map(int, input().split())
-arr = []
-st = res = 0
-for i in range(n):
-	arr.append(list(map(int, input().split())))
-	r = 1e10
-	for j in range(m):
-		if r > arr[i][j]:
-			r = arr[i][j]
-			yt = i
-	if st < r:
-		st = r
-		y = yt
+s = '-'+'-'.join(input())+'-'
+maxi = 0
+d = len(s)
+for i in range(d):
+	n = -1
+	for j in range(min(i, d-i-1)+1):
+		if s[i-j] == s[i+j]:
+			n += 1
+		else:
+			break
+	maxi = max(maxi, n)
+print(maxi)
 
-for i in range(m):
-	if st == arr[y][i]:
-		for j in range(n):
-			if st < arr[j][i]:
-				break
-			if j == n-1:
-				res = str(st)+'\n'+str(y)+' '+str(i)
-print(res)
+
+
+
+
+
+
+#s = input()
+#maxi = 0
+#for i in range(len(s)):
+#	for j in range(i, len(s)):
+#		test = s[i:j+1]
+#		if test == test[::-1]:
+#			maxi = max(maxi, (len(test)))
+#print(maxi)
