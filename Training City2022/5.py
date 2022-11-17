6ae3c05b42018f247d913061879e6ef77d8afdef
Nikita-2007 <nikita2007ua@gmail.com>
2022-11-17 21:33:45 +0300

    Update 5.py

--- Training City2022/5.py
+++ Training City2022/5.py
@@ -1,21 +1,29 @@
 s = '-'+'-'.join(input())+'-'
-maxi = 0
 d = len(s)
+arr = [0]*d
+maxi = c = r = 0
 for i in range(d):
-	n = -1
-	for j in range(min(i, d-i-1)+1):
-		if s[i-j] == s[i+j]:
-			n += 1
-		else:
-			break
-	maxi = max(maxi, n)
+	j = 0 if i > c+r else min(arr[c-(i-c)], c+r-i)
+	while i-j>0 and i+j<d-1 and s[i-j-1] == s[i+j+1]:
+		j+= 1
+	if j+i > c+r:
+		c, r = i, j
+	arr[i] = j
+	maxi = max(maxi, j)
 print(maxi)
 
-
-
-
-
-
+#s = '-'+'-'.join(input())+'-'
+#maxi = 0
+#d = len(s)
+#for i in range(d):
+#	n = -1
+#	for j in range(min(i, d-i-1)+1):
+#		if s[i-j] == s[i+j]:
+#			n += 1
+#		else:
+#			break
+#	maxi = max(maxi, n)
+#print(maxi)
 
 #s = input()
 #maxi = 0
