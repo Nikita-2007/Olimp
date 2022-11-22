a55e639e3ac035c7cf79ed6617392eddd5d2c619
Nikita-2007 <nikita2007ua@gmail.com>
2022-11-22 20:43:38 +0300

    Update 3.py

--- City 2021/3.py
+++ City 2021/3.py
@@ -3,14 +3,17 @@ x, y = map(int, input().split())
 x -= 1
 y -= 1
 k = int(input())
-arr = [[[] for j in range(m)] for i in range(n)]
+arr = dict()
 res = 'SUCCESS'
 for i in range(k):
 	a, b, c = input().split()
 	a, b = int(a)-1, int(b)-1
-	if c not in arr[a][b]:
+	if a not in arr.keys():
+		arr[a] = dict()
+	if a in arr.keys() and b not in arr[a]:
+		arr[a][b] = []
+	if a in arr.keys() and b in arr[a] and c not in arr[a][b]:
 		arr[a][b].append(c)
-	
 	if c == 'U' and a-1 >= 0:
 		c = 'D'
 		a -= 1
@@ -23,14 +26,17 @@ for i in range(k):
 	elif c == 'D' and a+1 < n:
 		c = 'U'
 		a += 1
-	if c not in arr[a][b]:
+	if a not in arr.keys():
+		arr[a] = dict()
+	if a in arr.keys() and b not in arr[a]:
+		arr[a][b] = []
+	if a in arr.keys() and b in arr[a] and c not in arr[a][b]:
 		arr[a][b].append(c)
-
+		
 for i in input():
-    for j in arr[x][y]:
-        if i==j:
-            res='FAIL'
-            break
+    if x in arr.keys() and y in arr[x].keys() and i in arr[x][y]:
+        res='FAIL'
+        break
     if res=='FAIL':
         break
     if i=='U':
@@ -45,3 +51,123 @@ for i in input():
         res='FAIL'
         break
 print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#n, m = map(int, input().split())
+#x, y = map(int, input().split())
+#x -= 1
+#y -= 1
+#k = int(input())
+#arr = []
+#res = 'SUCCESS'
+#for i in range(k):
+#	a, b, c = input().split()
+#	a, b = int(a)-1, int(b)-1
+#	abc = [a, b, c]
+#	if abc not in arr:
+#		arr.append(abc)		
+#	if c == 'U' and a-1 >= 0:
+#		c = 'D'
+#		a -= 1
+#	elif c == 'L' and b-1 >= 0:
+#		c = 'R'
+#		b -= 1
+#	elif c == 'R' and b+1 < m:
+#		c = 'L'
+#		b += 1
+#	elif c == 'D' and a+1 < n:
+#		c = 'U'
+#		a += 1
+#	abc = [a, b, c]
+#	if abc not in arr:
+#		arr.append(abc)	
+#
+#for i in input():
+#    for abc in arr:
+#        if x==abc[0] and y==abc[1] and i==abc[2]:
+#            res='FAIL'
+#            break
+#    if res=='FAIL':
+#        break
+#    if i=='U':
+#        x-=1
+#    elif i=='L':
+#        y-=1
+#    elif i=='R':
+#        y+=1
+#    elif i=='D':
+#        x+=1
+#    if x<0 or x>=n or y<0 or y>=m:
+#        res='FAIL'
+#        break
+#print(res)
+
+
+
+
+
+
+
+
+
+
+#Лимит памяти
+#n, m = map(int, input().split())
+#x, y = map(int, input().split())
+#x -= 1
+#y -= 1
+#k = int(input())
+#arr = [[[] for j in range(m)] for i in range(n)]
+#res = 'SUCCESS'
+#for i in range(k):
+#	a, b, c = input().split()
+#	a, b = int(a)-1, int(b)-1
+#	if c not in arr[a][b]:
+#		arr[a][b].append(c)
+#	
+#	if c == 'U' and a-1 >= 0:
+#		c = 'D'
+#		a -= 1
+#	elif c == 'L' and b-1 >= 0:
+#		c = 'R'
+#		b -= 1
+#	elif c == 'R' and b+1 < m:
+#		c = 'L'
+#		b += 1
+#	elif c == 'D' and a+1 < n:
+#		c = 'U'
+#		a += 1
+#	if c not in arr[a][b]:
+#		arr[a][b].append(c)
+#
+#for i in input():
+#    for j in arr[x][y]:
+#        if i==j:
+#            res='FAIL'
+#            break
+#    if res=='FAIL':
+#        break
+#    if i=='U':
+#        x-=1
+#    elif i=='L':
+#        y-=1
+#    elif i=='R':
+#        y+=1
+#    elif i=='D':
+#        x+=1
+#    if x<0 or x>=n or y<0 or y>=m:
+#        res='FAIL'
+#        break
+#print(res)
