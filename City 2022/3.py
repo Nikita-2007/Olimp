e7a9f4b221f900075497a4175c0d4db842d7f00d
Nikita-2007 <nikita2007ua@gmail.com>
2022-12-24 21:22:11 +0300

    Update 3.py

--- City 2022/3.py
+++ City 2022/3.py
@@ -3,12 +3,29 @@ arr = list(map(int, input().split()))
 s = t = 0
 for i in range(n):
 	s += arr[i] * (i+1)
+x = s
 for i in range(n):
-	for j in range(n):
-		arr[i], arr[j] = arr[j], arr[i]
-		for k in range(n):
-			t += arr[k] * (k+1)
-		s = max(s, t)
-		t = 0
-		arr[i], arr[j] = arr[j], arr[i]
-print(s)
+	for j in range(i, n):
+		t = s - (arr[i]*(i+1) + arr[j]*(j+1)) + (arr[i]*(j+1) + arr[j]*(i+1)) 
+		x = max(t, x)
+print(x)
+
+
+
+
+
+
+#n = int(input())
+#arr = list(map(int, input().split()))
+#s = t = 0
+#for i in range(n):
+#	s += arr[i] * (i+1)
+#for i in range(n):
+#	for j in range(n):
+#		arr[i], arr[j] = arr[j], arr[i]
+#		for k in range(n):
+#			t += arr[k] * (k+1)
+#		s = max(s, t)
+#		t = 0
+#		arr[i], arr[j] = arr[j], arr[i]
+#print(s)
